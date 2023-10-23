f5e2f511d35aa9516d70e1060d450b3914190e6c
Khodatskaa <khodatskaalina@gmail.com>
2023-10-23 19:17:59 +0300

    Ex 1 was done

--- cpp_app_6/main.cpp
+++ cpp_app_6/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
+	double profits[6];
+	double totalProfit = 0;
+
+	for (int i = 0; i < 6; ++i)
+	{
+		cout << "Enter profit for 1 month" << i + 1 << ": ";
+		cin >> profits[i];
+		totalProfit += profits[i];
+	}
+	cout << "Total profit for 6 months: " << totalProfit << endl;
 
 	return 0;
 }
